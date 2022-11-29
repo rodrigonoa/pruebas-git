4fe977b119418d3bf42fb5e449f1c92b1610f1d4
Hyakk1900 <alfrefrank1111@gmail.com>
2022-11-29 16:50:05 -0500

    editwllpaper

---
+++ resources/views/yewlcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testgitprron</title>
+</head>
+<body>
+    ><h1>
+        <img src="https://wallpapers.pictures/media/rocky-mountain-national-park-wallpaper-background-3840x2160.jpg" height="900" width="1600">
+    </h1>
+</body>
+</html>
