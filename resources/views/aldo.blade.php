fc455222dead9dd2d2fc3b1d07e77d76738460e4
Aldo Gallegos <pedroaldo14@gmail.com>
2022-11-29 16:39:20 -0600

    Nuevos cambios

--- resources/views/aldo.blade.php
+++ resources/views/aldo.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     Testeando vistas
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex exercitationem, voluptatibus officia ipsam obcaecati hic dolore nostrum cum! Sit quod veritatis recusandae earum accusantium qui odio voluptatum eligendi nesciunt totam vitae ut assumenda aliquam cumque soluta exercitationem ad, possimus error? Minima sequi corporis est magnam veniam dignissimos, autem illum quos cum. Mollitia, saepe sint dignissimos quis illum numquam iusto culpa voluptates incidunt eius esse fugit, ipsam quas dolorem perspiciatis amet. Quia cupiditate pariatur sit dicta, aliquam quaerat repellendus, odio qui modi cum recusandae provident placeat, similique optio culpa numquam deleniti. Dolor adipisci amet placeat, molestias fuga numquam deleniti labore totam?</p>
+    <li>Testeando</li>
+    <li>Testeando</li>
+    <li>Testeando</li>
+    <li>Testeando</li>
 </body>
 </html>
